893396d4cac1e09dfb1ee55a8d8eee0cd3dfc99d
helloyong <2628578405@qq.com>
2018-10-05 21:37:00 +0800

    add full files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+import { Button } from 'antd'
+import { createStore } from 'redux'
+
+//reducers
+const defaultState = {
+  number: 1
+}
+function reducer(state = defaultState, action) {
+  if (action.type === 'add_number') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.number += 1
+    return newState
+  }
+  if (action.type === 'sub_number') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.number -= 1
+    return newState
+  }
+  return state
+}
+
+//store
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = store.getState()
+    store.subscribe(this.handleUpdate.bind(this))
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div>
+        <div className="App" style={{marginLeft: '10px', marginBottom: '20px'}}>
+          <div>{this.state.number}</div>
+          <Button
+            type="primary"
+            style={{marginRight: '10px'}}
+            onClick={this.handleAdd.bind(this)}
           >
-            Learn React
-          </a>
-        </header>
+            +
+          </Button>
+          <Button
+            type="primary"
+            onClick={this.handleSub.bind(this)}
+          >
+            -
+          </Button>
+        </div>
       </div>
-    );
+    )
+  }
+  handleUpdate() {
+    this.setState(store.getState())
+  }
+  handleAdd() {
+    const action = {
+      type: 'add_number',
+    }
+    store.dispatch(action)
+  }
+  handleSub() {
+    const action = {
+      type: 'sub_number',
+    }
+    store.dispatch(action)
   }
 }
 
